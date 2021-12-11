c3837e409763355bc2371ff6a0c70bcd6f63e8de
marcpaulo15 <94222660+marcpaulo15@users.noreply.github.com>
2021-12-11 22:08:07 +0100

    Add files via upload

---
+++ TextModel/TextFunction.py
@@ -0,0 +1,37 @@
+
+import regex
+import nltk
+#nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+
+# dictionary containing all emojis.
+emojis = {':)': 'smile', ':-)': 'smile', ';d': 'wink', ':-E': 'vampire', ':(': 'sad', 
+          ':-(': 'sad', ':-<': 'sad', ':P': 'raspberry', ':O': 'surprised',
+          ':-@': 'shocked', ':@': 'shocked',':-$': 'confused', ':\\': 'annoyed', 
+          ':#': 'mute', ':X': 'mute', ':^)': 'smile', ':-&': 'confused', '$_$': 'greedy',
+          '@@': 'eyeroll', ':-!': 'confused', ':-D': 'smile', ':-0': 'yell', 'O.o': 'confused',
+          '<(-_-)>': 'robot', 'd[-_-]b': 'dj', ":'-)": 'sadsmile', ';)': 'wink', 
+          ';-)': 'wink', 'O:-)': 'angel','O*-)': 'angel','(:-D': 'gossip', '=^.^=': 'cat'}
+
+##Function to clean the data.
+def preprocess(textdata , wordLemm):
+    processedText = []
+    
+    
+    for tweet in textdata:
+        tweet = tweet.lower()
+
+        # Replace all emojis.
+        for emoji in emojis.keys():
+            tweet = tweet.replace(emoji, "EMOJI" + emojis[emoji])        
+        
+        tweetwords = ''
+        for word in tweet.split():
+            if len(word)>1:
+                # Lemmatizing the word.
+                word = wordLemm.lemmatize(word)
+                tweetwords += (word+' ')
+            
+        processedText.append(tweetwords)
+        
+    return processedText
